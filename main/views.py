111acd0c4440eb095513ec3ddd12e616918a7e34
JackS0l0 <you@example.com>
2024-08-01 15:29:32 +0400

    modified:   db.sqlite3 	modified:   main/__pycache__/views.cpython-312.pyc 	modified:   main/views.py 	modified:   templates/index.html

--- main/views.py
+++ main/views.py
@@ -7,5 +7,9 @@ def home(request):
         'trend': Trend.objects.all().order_by('-date'),
         'categories': Categories.objects.all().order_by('name'),
         'article_first_cover': Article.objects.all().order_by('-date')[0:1],
+        'article_second_cover': Article.objects.all().order_by('-date')[1:2],
+        'article_third_cover': Article.objects.all().order_by('-date')[2:3],
+        'article_fourth_cover': Article.objects.all().order_by('-date')[3:4],
+        'article_mob_cover': Article.objects.all().order_by('-date')[0:4],
     }
     return render(request,'index.html',data)

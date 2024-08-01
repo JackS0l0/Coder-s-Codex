4579e1c3b5226738e655339ce7578d2bb07871e6
JackS0l0 <you@example.com>
2024-08-01 16:53:18 +0400

    modified:   app/__pycache__/settings.cpython-312.pyc 	modified:   app/__pycache__/urls.cpython-312.pyc 	modified:   app/settings.py 	modified:   app/urls.py 	new file:   locale/en/LC_MESSAGES/django.po 	new file:   locale/ru/LC_MESSAGES/django.po 	new file:   main/__pycache__/translation.cpython-312.pyc 	modified:   main/__pycache__/views.cpython-312.pyc 	new file:   main/translation.py 	modified:   main/views.py 	modified:   templates/base.html

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.views.static import serve
 from main import views as mainviews
 from articles import views as articlesviews
+from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
@@ -12,3 +13,7 @@ urlpatterns = [
     path('',mainviews.home,name='homepage'),
     path('article/<int:pk>/',articlesviews.ArticleDetail.as_view(),name='articledetail')
 ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns = [
+    *i18n_patterns(*urlpatterns, prefix_default_language=False),
+    path("set_language/<str:language>", mainviews.set_language, name="set-language"),
+] 
--- main/views.py
+++ main/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from articles.models import Topadvertising,Trend,Categories,Article
+from urllib.parse import urlparse
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.urls.base import resolve, reverse
+from django.urls.exceptions import Resolver404
+from django.utils import translation
+def set_language(request, language):
+    for lang, _ in settings.LANGUAGES:
+        translation.activate(lang)
+        try:
+            view = resolve(urlparse(request.META.get("HTTP_REFERER")).path)
+        except Resolver404:
+            view = None
+        if view:
+            break
+    if view:
+        translation.activate(language)
+        next_url = reverse(view.url_name, args=view.args, kwargs=view.kwargs)
+        response = HttpResponseRedirect(next_url)
+        response.set_cookie(settings.LANGUAGE_COOKIE_NAME, language)
+    else:
+        response = HttpResponseRedirect("/")
+    return response
 def home(request):
     data={
         'title':"Coder's Code",
